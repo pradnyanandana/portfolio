e4c7d55acd2b141d8a4cd6ddc5fcdbfabd8cedd9
Pradnyanandana <pradnyanandana@gmail.com>
2021-10-13 23:36:44 +0800

    Animation

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,19 +1,20 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {FaTwitter ,FaInstagram ,FaFacebookF , FaLinkedinIn } from "react-icons/fa";
 import {FiAlignRight, FiX} from "react-icons/fi";
-import {Link} from 'react-scroll'
+import {Link} from 'react-scroll';
+
+const SocialShare = [
+    {Social: <FaFacebookF /> , link: 'https://www.facebook.com/'},
+    {Social: <FaLinkedinIn /> , link: 'https://www.linkedin.com/'},
+    {Social: <FaInstagram /> , link: 'https://www.instagram.com/'},
+    {Social: <FaTwitter /> , link: 'https://twitter.com/'},
+];
+
+const CurentMenuAfterClass = "after:content after:w-full after:absolute after:left-0 after:bottom-0 after:h-0.5 after:bg-orange-500";
 
 const Header = (props) => {
     const [openMenu, setOpenMenu] = useState(false);
-
-    const SocialShare = [
-        {Social: <FaFacebookF /> , link: 'https://www.facebook.com/'},
-        {Social: <FaLinkedinIn /> , link: 'https://www.linkedin.com/'},
-        {Social: <FaInstagram /> , link: 'https://www.instagram.com/'},
-        {Social: <FaTwitter /> , link: 'https://twitter.com/'},
-    ];
-
-    const CurentMenuAfterClass = "after:content after:w-full after:absolute after:left-0 after:bottom-0 after:h-0.5 after:bg-orange-500";
+    const [headerBgOpacity, setHeaderBgOpacity] = useState("0");
 
     const OpenMenu = () => {
         setOpenMenu(true);
@@ -23,8 +24,18 @@ const Header = (props) => {
         setOpenMenu(false);
     }
 
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            window.addEventListener("scroll", () => {
+                let opacity = (window.pageYOffset / 500).toFixed(2);
+                opacity = opacity > 1 ? 1 : opacity;
+                setHeaderBgOpacity(opacity.toString())
+            });
+        }
+    }, []);
+
     return (
-        <header className="header-area absolute z-50 top-0 bloc w-full bg-transparent">
+        <header className={`header-area fixed z-50 top-0 bloc w-full transition-all bg-white`} style={{"--tw-bg-opacity": headerBgOpacity}}>
             <div className="header-wrapper container mx-auto px-2 md:px-4 py-3 xl:py-5 flex justify-between items-center flex-wrap">
                 <div className="header-left flex items-center">
                     <div className="header-logo block">
@@ -32,7 +43,7 @@ const Header = (props) => {
                     </div>
                     <nav className="main-menu-nav absolute xl:relative ml-20 xl:block">
                         <ul className={`main-menu block fixed top-0 ${openMenu ? "right-0" : "-right-full"} h-full w-4/6 z-40 py-10 px-8 bg-gray-900 xl:w-auto xl:h-auto xl:bg-transparent xl:p-0 xl:m-0 xl:flex xl:flex-wrap xl:justify-end xl:relative xl:right-0 xl:flex transition-all`}>
-                            <li className="py-2 xl:py-0 xl:ml-0 xl:mr-5 relative font-rubik cursor-pointer"><a data-content=" " className={`relative text-white xl:text-gray-900 font-semibold p-0 xl:py-5  text-xl xl:text-lg ${CurentMenuAfterClass}`} href="#home">Home</a></li>
+                            <li className="py-2 xl:py-0 xl:ml-0 xl:mr-5 relative font-rubik cursor-pointer"><a data-content=" " className={`relative text-white xl:text-gray-900 font-semibold p-0 xl:py-5 text-xl xl:text-lg ${CurentMenuAfterClass}`} href="#home">Home</a></li>
                             <li className="py-2 xl:py-0 xl:mx-3 relative font-rubik cursor-pointer"><Link to="about" spy={true} smooth={true} className="relative text-white xl:text-gray-700 xl:hover:text-gray-900 font-semibold p-0 xl:py-5 text-xl xl:text-lg">About</Link></li>
                             <li className="py-2 xl:py-0 xl:mx-3 relative font-rubik cursor-pointer"><Link to="service" spy={true} smooth={true} className="relative text-white xl:text-gray-700 xl:hover:text-gray-900 font-semibold p-0 xl:py-5 text-xl xl:text-lg">Service</Link></li>
                             <li className="py-2 xl:py-0 xl:mx-3 relative font-rubik cursor-pointer"><a data-content=" " className="relative text-white xl:text-gray-700 xl:hover:text-gray-900 font-semibold p-0 xl:py-5 text-xl xl:text-lg" href="#portfolio">Portfolio</a></li>
--- src/component/section/Hero.js
+++ src/component/section/Hero.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Lottie from "react-lottie";
 import animationData from "../../lottie/hero.json";
+import TextLoop from "react-text-loop";
+import {BsChevronDoubleDown} from "react-icons/bs";
+import {Link} from 'react-scroll';
 
 const defaultOptions = {
     loop: true,
@@ -11,19 +14,18 @@ const defaultOptions = {
     }
 };
 
+const rotatingText = ["Web", "Backend", "WordPress"]
+
 const Hero = (props) => {
     return (
-        <section id="hero" className="hero-3 pt-32 pb-12 lg:pb-40 lg:pt-48">
-            <div className="container px-2 md:px-4 mx-auto">
-                <div className="flex flex-wrap flex-col-reverse lg:flex-row items-center -mx-3">
+        <section id="hero" className="hero-3 h-screen relative">
+            <div className="container h-full px-2 md:px-4 mx-auto">
+                <div className="flex h-full flex-wrap flex-col-reverse justify-center lg:flex-row items-center -mx-3">
                     <div className="w-full lg:w-2/5 px-3">
                         <div className="max-w-lg lg:max-w-md mx-auto lg:mx-0 mb-8 text-center lg:text-left">
                             <h2 className="text-4xl lg:text-6xl mb-4 lg:leading-tight font-bold font-rubik wow animate__ animate__fadeIn animated visible">
-                                Hi, I'm Pradnya. a Software Developer.
+                                Hi, I'm Pradnya. <br/>a <TextLoop>{rotatingText.map((text, index) => (<span key={`rotating-text-${index}`} className="bg-orange-500 rounded-xl text-white px-3">{text}</span>))}</TextLoop> Developer.
                             </h2>
-                        </div>
-                        <div className="text-center lg:text-left">
-                            <a className="tracking-wide hover-up-2 block sm:inline-block py-4 px-8 mb-4 sm:mb-0 sm:mr-3 text-base text-white text-center font-semibold font-roboto leading-none bg-orange-500 hover:bg-yellow-600 uppercase rounded-xl wow animate__ animate__fadeInUp animated visible" href="#key-features">About Me</a>
                         </div>
                     </div>
                     <div className="w-full lg:w-3/5 px-3 mb-12 lg:mb-0">
@@ -38,6 +40,14 @@ const Hero = (props) => {
                     </div>
                 </div>
             </div>
+            <Link 
+                to="about" 
+                spy={true} 
+                smooth={true} 
+                className="absolute bottom-8 left-2/4 transform text-lg cursor-pointer animate-bounce" 
+                style={{"--tw-translate-x": "-50%"}}>
+                <BsChevronDoubleDown size={24}/>
+            </Link>
         </section>
     );
 };
