7b5804086e2be66e320b57aefe27a23b44d08553
Pradnyanandana Suwitra <pradnyanandana@gmail.com>
2022-11-10 23:59:29 +0800

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import PageHelmet from "./component/common/Helmet";
 import Header from "./component/header/Header";
 import Hero from "./component/section/Hero";
-import About from "./component/section/About"
+import About from "./component/section/About";
 import Service from "./component/section/Service";
 import Footer from "./component/footer/Footer";
 import Project from "./component/section/Project";
@@ -12,7 +12,7 @@ import "./App.css";
 const App = (props) => {
     return (
         <Fragment>
-            <PageHelmet pageTitle="Portfolio"/>
+            <PageHelmet pageTitle="Portfolio" />
             <Header />
             <Hero />
             <About />
