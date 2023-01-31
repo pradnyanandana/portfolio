0f586ca4f8b8a81bcaad91759471a5ac90cea9c8
Pradnyanandana Suwitra <pradnyanandana@gmail.com>
2023-01-31 22:07:44 +0800

    Animated logo

--- src/component/section/Project.js
+++ src/component/section/Project.js
@@ -70,13 +70,13 @@ const Project = (props) => {
         <section id="project" className="pt-16 pb-20">
             <div className="container px-2 md:px-4 mx-auto text-center sm:text-left">
                 <div className="flex flex-col sm:flex-row sm:items-center mb-8">
-                    <div className="w-full sm:w-4/12 lg:w-3/12">
+                    <div className="w-full w-4/12">
                         <span className="inline-block text-xs py-1 px-3 text-blue-500 font-semibold font-roboto bg-blue-100 rounded-xl wow animate__ animate__fadeInUp animated visible">Portfolio</span>
                         <h2 className="mx-auto text-3xl xl:text-4xl font-rubik my-4 font-bold font-heading wow animate__ animate__fadeInUp animated animated visible">
-                            <span>Some of My Latest Projects</span>
+                            <span>Some of <br/>My Latest Projects</span>
                         </h2>
                     </div>
-                    <div className="w-full sm:w-8/12 lg:w-9/12 sm:ml-12">
+                    <div className="w-full w-8/12 sm:ml-12">
                         <p className="mx-auto text-lg text-blueGray-400 font-roboto wow animate__ animate__fadeInUp animated animated visible">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque efficitur nisl sodales egestas lobortis.</p>
                     </div>
                 </div>
